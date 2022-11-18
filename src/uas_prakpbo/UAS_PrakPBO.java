6169dce5b9d5da4ea34f6ab4afd954dd32eeacf9
grald003 <josephgeraldo2003@gmail.com>
2022-11-18 12:34:48 +0700

    First commit

---
+++ src/uas_prakpbo/UAS_PrakPBO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas_prakpbo;
+
+/**
+ *
+ * @author user
+ */
+
+public class UAS_PrakPBO {
+    public static void main(String[] args) {
+        
+    } 
+}
